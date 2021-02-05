57ce3c8b45efd79fc281cc22353b573a9feafeab
DINLUKAC <DINLUKAC@DI06N00025>
2021-02-05 13:17:59 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,71 @@
+class Calculator{
+  getValues(){
+    var n1 = document.getElementById("number1").value;
+    var n2 = document.getElementById("number2").value;
+    console.log("First number's type in input: " + typeof n1);
+    console.log("Second number's type in input: " + typeof n1);
+
+
+    if(isNaN(n1) || isNaN(n2)){
+      console.error("Values have to be numbers.");
+    }
+
+    return this.convertToNumber(n1, n2);
+  }
+
+  updateResult(r){
+    document.getElementById("result").value = r;
+  }
+
+  convertToNumber(...strings){
+    console.log("...rest operator: " + strings);
+    var result = strings.map(function (string) {
+      var number = Number(string);
+      return number;
+    });
+
+    console.log("First number's type after converting: " + typeof result[0]);
+    console.log("Second number's type after converting: " + typeof result[1]);
+    return result;
+  }
+
+}
+
+let calculator = new Calculator();
+
+let methodSelect = document.getElementById("method");
+let equalsBtn = document.getElementById("equals");
+let method = methodSelect.value;
+const one = 1;
+
+methodSelect.addEventListener("change", () => { method = methodSelect.value; }, false);
+
+equalsBtn.addEventListener("click", () => {
+  var values = calculator.getValues();
+  plus();
+
+  switch (method) {
+    case '+':
+      calculator.updateResult(values[0] + values[1]);
+      break;
+    case '-':
+      calculator.updateResult(values[0] - values[1]);
+      break;
+    case '*':
+      calculator.updateResult(values[0] * values[1]);
+      break;
+    case '/':
+      calculator.updateResult(values[0] / values[1]);
+      break;
+  }
+}, false);
+
+let plus =(function(){
+ let sum = 0;
+
+return function(){
+  sum += one;
+  var times = sum > 1 ? sum + " times" : "once";
+  return console.log("Calculator has been used " + times);
+}
+})();
